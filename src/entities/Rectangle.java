4f11f8222387ad990cf417155b60be05cbabb4ee
lucio <lucianosathler2005@gmail.com>
2026-02-20 10:07:55 -0300

    Exercício do Retângulo concluído

---
+++ src/entities/Rectangle.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Rectangle {
+    public double width;
+    public double height;
+
+    public double area(){
+        return  width * height;
+    }
+    public double perimeter(){
+        return (width + height) * 2;
+    }
+    public double diagonal(){
+        return Math.sqrt(Math.pow(width, 2.0) + Math.pow(height, 2.0));
+    }
+    public String toString() {
+        return "AREA = " + String.format("%.2f", area())
+                + "\nPERIMETER = " + String.format("%.2f", perimeter())
+                + "\nDIAGONAL = " + String.format("%.2f", diagonal());
+    }
+}
